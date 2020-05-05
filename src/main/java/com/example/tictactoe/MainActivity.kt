c94da01963a97244f724b5d935ec36c0cdbec867
jakub <duduskuba@gmail.com>
2020-05-05 02:45:47 +0200

    Working hotseat

--- src/main/java/com/example/tictactoe/MainActivity.kt
+++ src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,21 +1,25 @@
 package com.example.tictactoe
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar!!.hide()
         setContentView(R.layout.activity_main)
-        btn.setOnClickListener{
-
+        btn_2.setOnClickListener{
             val intent = Intent (this,NewActivity::class.java)
-            intent.putExtra("computer", switch1.isChecked)
+                //intent.putExtra("computer", switch1.isChecked)
             startActivity(intent)
+        }
+        btn_4.setOnClickListener{
+            finish()
+            exitProcess(0);
         }
     }
 }
--- src/main/java/com/example/tictactoe/NewActivity.kt
+++ src/main/java/com/example/tictactoe/NewActivity.kt
@@ -1,17 +1,18 @@
 package com.example.tictactoe
 
 import android.annotation.SuppressLint
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import android.content.res.Configuration
-import android.os.PersistableBundle
-import android.view.WindowManager
 
 
-@Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+@Suppress(
+    "RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS",
+    "NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS"
+)
 class NewActivity : AppCompatActivity() {
 
     private var buttons = arrayOfNulls<Button>(100)
@@ -21,29 +22,38 @@ class NewActivity : AppCompatActivity() {
     private var p2Score = 0
     private lateinit var tvp1: TextView
     private lateinit var tvp2: TextView
-    private var computer: Boolean = false
+    // private var computer: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
-        this.tvp1 = findViewById(R.id.text_view_p1)
-        this.tvp2 = findViewById(R.id.text_view_p2)
-        computer = this.intent.extras.getBoolean("computer")
 
-        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        } else {
-            window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        }
+        tvp1 = findViewById(R.id.text_view_p1)
+        tvp2 = findViewById(R.id.text_view_p2)
 
+        // computer = this.intent.extras!!.getBoolean("computer")
+
+        // if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        //    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        // } else {
+        //     window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        //  }
+        supportActionBar!!.hide()
         turn = savedInstanceState?.getBoolean("turn") ?: true
+        switchTurn()
         for (x in 0..99) {
             val btn = "button_$x"
             val resID = resources.getIdentifier(btn, "id", packageName)
             buttons[x] = findViewById(resID)
             buttons[x]!!.setOnClickListener {
                 if ((buttons[x] as Button).text.toString() != "") return@setOnClickListener
-                if (turn) buttons[x]!!.text = "X" else buttons[x]!!.text = "O"
+                if (turn) {
+                    buttons[x]!!.text = "X"
+                    buttons[x]!!.isActivated = true
+                } else {
+                    buttons[x]!!.text = "O"
+                    buttons[x]!!.isEnabled = false
+                }
                 roundCount++
 
                 if (checkForWin()) {
@@ -55,8 +65,8 @@ class NewActivity : AppCompatActivity() {
                 } else if (roundCount == 99) {
                     draw()
                 } else {
-                    turn = !turn
-                    if (computer && !turn) cpu()
+                   switchTurn()
+                    // if (computer && !turn) cpu()
                 }
             }
         }
@@ -68,15 +78,15 @@ class NewActivity : AppCompatActivity() {
 
 
     private fun checkForWin(): Boolean {
-        return(chceckHorizontal() || chceckVertical())
+        // Log.i("win ? ", (checkHorizontal() || checkVertical()).toString())
+        return (checkHorizontal() || checkVertical())
     }
 
-    private fun chceckHorizontal(): Boolean {
+    private fun checkVertical(): Boolean {
         for (x in 4..94 step 10) {
             // Czy dwa środkowe mają takie same niepuste znaki
-            if (buttons[x]!!.text.toString() != "" && buttons[x + 1]!!.text.toString() != "" && buttons[x]!!.text.toString() == buttons[x + 1]!!.text.toString()) {
-                // Sprawdzamy wszystkie możliwości zawierające 2 środkowe znaki
-                return ((buttons[x - 3]!!.text.toString() == buttons[x]!!.text.toString() &&
+            if (buttons[x]!!.text.toString() != "" && buttons[x]!!.text.toString() == buttons[x + 1]!!.text.toString() &&
+                ((buttons[x - 3]!!.text.toString() == buttons[x]!!.text.toString() &&
                         buttons[x - 2]!!.text.toString() == buttons[x]!!.text.toString() &&
                         buttons[x - 1]!!.text.toString() == buttons[x]!!.text.toString() &&
                         buttons[x + 1]!!.text.toString() == buttons[x]!!.text.toString()) ||
@@ -92,38 +102,35 @@ class NewActivity : AppCompatActivity() {
                                 buttons[x + 2]!!.text.toString() == buttons[x]!!.text.toString() &&
                                 buttons[x + 3]!!.text.toString() == buttons[x]!!.text.toString() &&
                                 buttons[x + 4]!!.text.toString() == buttons[x]!!.text.toString()))
-            }
+            ) return true
             // Jeśli lewy środek jest zaznaczony i jest różny od prawgo środka
-            if (buttons[x]!!.text.toString() != "") {
-                if (    buttons[x - 4]!!.text.toString() == buttons[x]!!.text.toString() &&
-                        buttons[x - 3]!!.text.toString() == buttons[x]!!.text.toString() &&
-                        buttons[x - 2]!!.text.toString() == buttons[x]!!.text.toString() &&
-                        buttons[x - 1]!!.text.toString() == buttons[x]!!.text.toString())
-                    return true
-
-            }
-            if (buttons[x + 1]!!.text.toString() != "") { // Jeśli prawy środek jest zaznaczony i jest różny od lewego środka
-                return (
-                        buttons[x + 5]!!.text.toString() == buttons[x + 1]!!.text.toString() &&
-                                buttons[x + 4]!!.text.toString() == buttons[x + 1]!!.text.toString() &&
-                                buttons[x + 3]!!.text.toString() == buttons[x + 1]!!.text.toString() &&
-                                buttons[x + 2]!!.text.toString() == buttons[x + 1]!!.text.toString())
-            }
-
-
+            if (buttons[x]!!.text.toString() != "" &&
+                buttons[x - 4]!!.text.toString() == buttons[x]!!.text.toString() &&
+                buttons[x - 3]!!.text.toString() == buttons[x]!!.text.toString() &&
+                buttons[x - 2]!!.text.toString() == buttons[x]!!.text.toString() &&
+                buttons[x - 1]!!.text.toString() == buttons[x]!!.text.toString()
+            ) return true
+            // Jeśli prawy środek jest zaznaczony i jest różny od lewego środka
+            if (buttons[x + 1]!!.text.toString() != "" &&
+                buttons[x + 5]!!.text.toString() == buttons[x + 1]!!.text.toString() &&
+                buttons[x + 4]!!.text.toString() == buttons[x + 1]!!.text.toString() &&
+                buttons[x + 3]!!.text.toString() == buttons[x + 1]!!.text.toString() &&
+                buttons[x + 2]!!.text.toString() == buttons[x + 1]!!.text.toString()
+            )
+                return true
         }
         return false
     }
 
-    private fun chceckVertical(): Boolean {
+    private fun checkHorizontal(): Boolean {
         for (x in 40..49) {
             // Czy dwa środkowe mają takie same niepuste znaki
-            if (buttons[x]!!.text.toString() != "" && buttons[x + 10]!!.text.toString() != "" && buttons[x]!!.text.toString() == buttons[x + 10]!!.text.toString()) {
-                // Sprawdzamy wszystkie możliwości zawierające 2 środkowe znaki
-                return ((buttons[x - 30]!!.text.toString() == buttons[x]!!.text.toString() &&
-                        buttons[x - 20]!!.text.toString() == buttons[x]!!.text.toString() &&
-                        buttons[x - 10]!!.text.toString() == buttons[x]!!.text.toString() &&
-                        buttons[x + 10]!!.text.toString() == buttons[x]!!.text.toString()) ||
+            if (buttons[x]!!.text.toString() != "" && buttons[x]!!.text.toString() == buttons[x + 10]!!.text.toString() &&
+                (
+                        (buttons[x - 30]!!.text.toString() == buttons[x]!!.text.toString() &&
+                                buttons[x - 20]!!.text.toString() == buttons[x]!!.text.toString() &&
+                                buttons[x - 10]!!.text.toString() == buttons[x]!!.text.toString() &&
+                                buttons[x + 10]!!.text.toString() == buttons[x]!!.text.toString()) ||
                         (buttons[x - 20]!!.text.toString() == buttons[x]!!.text.toString() &&
                                 buttons[x - 10]!!.text.toString() == buttons[x]!!.text.toString() &&
                                 buttons[x + 10]!!.text.toString() == buttons[x]!!.text.toString() &&
@@ -135,26 +142,25 @@ class NewActivity : AppCompatActivity() {
                         (buttons[x + 10]!!.text.toString() == buttons[x]!!.text.toString() &&
                                 buttons[x + 20]!!.text.toString() == buttons[x]!!.text.toString() &&
                                 buttons[x + 30]!!.text.toString() == buttons[x]!!.text.toString() &&
-                                buttons[x + 40]!!.text.toString() == buttons[x]!!.text.toString()))
-            }
+                                buttons[x + 40]!!.text.toString() == buttons[x]!!.text.toString())
+                 )
+            ) return true
+
             // Jeśli górny środek jest zaznaczony i jest różny od dolnego środka
-            if (buttons[x]!!.text.toString() != "") {
-                if (    buttons[x - 40]!!.text.toString() == buttons[x]!!.text.toString() &&
-                        buttons[x - 30]!!.text.toString() == buttons[x]!!.text.toString() &&
-                        buttons[x - 20]!!.text.toString() == buttons[x]!!.text.toString() &&
-                        buttons[x - 10]!!.text.toString() == buttons[x]!!.text.toString())
-                    return true
+            if (buttons[x]!!.text.toString() != "" &&
+                buttons[x - 40]!!.text.toString() == buttons[x]!!.text.toString() &&
+                buttons[x - 30]!!.text.toString() == buttons[x]!!.text.toString() &&
+                buttons[x - 20]!!.text.toString() == buttons[x]!!.text.toString() &&
+                buttons[x - 10]!!.text.toString() == buttons[x]!!.text.toString()
+            ) return true
 
-            }
-            if (buttons[x + 10]!!.text.toString() != "") { // Jeśli dolny środek jest zaznaczony i jest różny od górnego środka
-                return (
-                        buttons[x + 50]!!.text.toString() == buttons[x + 10]!!.text.toString() &&
-                                buttons[x + 40]!!.text.toString() == buttons[x + 10]!!.text.toString() &&
-                                buttons[x + 30]!!.text.toString() == buttons[x + 10]!!.text.toString() &&
-                                buttons[x + 20]!!.text.toString() == buttons[x + 10]!!.text.toString())
-            }
-
-
+            // Jeśli dolny środek jest zaznaczony i jest różny od górnego środka
+            if (buttons[x + 10]!!.text.toString() != "" &&
+                buttons[x + 50]!!.text.toString() == buttons[x + 10]!!.text.toString() &&
+                buttons[x + 40]!!.text.toString() == buttons[x + 10]!!.text.toString() &&
+                buttons[x + 30]!!.text.toString() == buttons[x + 10]!!.text.toString() &&
+                buttons[x + 20]!!.text.toString() == buttons[x + 10]!!.text.toString()
+            ) return true
         }
         return false
     }
@@ -163,8 +169,7 @@ class NewActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     private fun updatePointsText() {
         tvp1.text = "Player 1: $p1Score"
-        if (computer) tvp2.text = "Computer: $p2Score"
-        else tvp2.text = "Player 2 : $p2Score"
+        tvp2.text = "Player 2 : $p2Score"
     }
 
     private fun resetGame() {
@@ -175,9 +180,25 @@ class NewActivity : AppCompatActivity() {
     }
 
     private fun resetBoard() {
-        for (i in 0..24) buttons[i]!!.text = ""
+        for (i in 0..99) {
+            buttons[i]!!.text = ""
+            buttons[i]!!.isActivated = false
+            buttons[i]!!.isEnabled = true
+        }
         roundCount = 0
-        turn = true
+       switchTurn()
+    }
+    private fun switchTurn() {
+        turn = !turn
+        if(turn){
+            tvp1.isEnabled=true
+            tvp2.isEnabled=false
+        }
+        else{
+            tvp1.isEnabled=false
+            tvp2.isEnabled=true
+        }
+
     }
 
     private fun player1Wins() {
@@ -199,38 +220,38 @@ class NewActivity : AppCompatActivity() {
         resetBoard()
     }
 
-    private fun cpu() {
-        var arr = arrayOf(-6, -5, -4, -1, 1, 4, 5, 6)
-        if (roundCount == 1) {
-            var rand = (0..24).random()
-            while (buttons[rand]!!.text != "") {
-                rand = (0..24).random()
-            }
-            buttons[rand]!!.performClick()
-        } else {
-            for (i in 0..24) {
-                if (buttons[i]!!.text == "O") {
-                    if (i == 9 || i == 14 || i == 19) arr = arrayOf(-6, -5, -1, 4, 5)
-                    else if (i == 5 || i == 10 || i == 15) arr = arrayOf(6, -5, 1, -4, 5)
-                    else if (i == 2 || i == 3 || i == 1) arr = arrayOf(1, -1, 4, 5, 6)
-                    else if (i == 21 || i == 22 || i == 23) arr = arrayOf(1, -1, -4, -5, -6)
-                    else if (i == 0) arr = arrayOf(1, 5, 6)
-                    else if (i == 24) arr = arrayOf(-1, -5, -6)
-                    else if (i == 4) arr = arrayOf(-1, 5, 4)
-                    else if (i == 20) arr = arrayOf(1, -5, -4)
-                    for (j in arr) {
-                        val x = i + arr.random()
-                        if (x in 0..24) {
-                            if (buttons[x]!!.text == "") {
-                                buttons[x]!!.performClick()
-                                return
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+//    private fun cpu() {
+//        var arr = arrayOf(-6, -5, -4, -1, 1, 4, 5, 6)
+//        if (roundCount == 1) {
+//            var rand = (0..24).random()
+//            while (buttons[rand]!!.text != "") {
+//                rand = (0..24).random()
+//            }
+//            buttons[rand]!!.performClick()
+//        } else {
+//            for (i in 0..24) {
+//                if (buttons[i]!!.text == "O") {
+//                    if (i == 9 || i == 14 || i == 19) arr = arrayOf(-6, -5, -1, 4, 5)
+//                    else if (i == 5 || i == 10 || i == 15) arr = arrayOf(6, -5, 1, -4, 5)
+//                    else if (i == 2 || i == 3 || i == 1) arr = arrayOf(1, -1, 4, 5, 6)
+//                    else if (i == 21 || i == 22 || i == 23) arr = arrayOf(1, -1, -4, -5, -6)
+//                    else if (i == 0) arr = arrayOf(1, 5, 6)
+//                    else if (i == 24) arr = arrayOf(-1, -5, -6)
+//                    else if (i == 4) arr = arrayOf(-1, 5, 4)
+//                    else if (i == 20) arr = arrayOf(1, -5, -4)
+//                    for (j in arr) {
+//                        val x = i + arr.random()
+//                        if (x in 0..24) {
+//                            if (buttons[x]!!.text == "") {
+//                                buttons[x]!!.performClick()
+//                                return
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
 
     override fun onSaveInstanceState(outState: Bundle?, outPersistentState: PersistableBundle?) {
         super.onSaveInstanceState(outState, outPersistentState)
